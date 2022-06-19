aca321284ca4e785042a9bcb46840f2db9ee97a7
levibb <101865688+levibb@users.noreply.github.com>
2022-06-20 00:26:27 +0300

    infinite scroll will call API just once when response data is empty
    Signed-off-by: levibb <101865688+levibb@users.noreply.github.com>

--- control_beer/src/browse_beers/BrowseBeers.js
+++ control_beer/src/browse_beers/BrowseBeers.js
@@ -32,28 +32,36 @@ export function BrowseBeers(props) {
 
     const beers = useSelector((state) => state.beers);
     const [loading, setLoading] = useState(true);
+    const [next, setNext] = useState(true);
     const dispatch = useDispatch();
     const [page, setPage] = useState(1)
 
     // this function send API request with page number and send it the beer's global state and add it to prev data.
     function loadData(){
+        if (next){
         axios.get(BASE_URL+'?page='+page+'&per_page=12')
         .then((response) => {
+            if (response.data.length === 0){
+                setNext(false)
+                console.log('end of data - no next')
+                {return}
+            }
             dispatch(getData([...response.data]));
             setLoading(false)
             console.log('API called',response)
         });
-    }
+    }}
 
     // this calculate the point where the user reached the bottom of page and need to load more data, by updating page value
     const handleScroll = (e) => {
         let top = e.target.documentElement.scrollTop
         let win = window.innerHeight
         let height = e.target.documentElement.scrollHeight
-        if (top + win + 1 >= height){
+        if ((top + win + 1 >= height) && (next)){
             console.log('got to bottom of the page')
             setPage((prevState) => (prevState+1))
-            setLoading(true);
+            setLoading(true)
+            console.log(next) // error -doent change to false - check
             }
         }
 
